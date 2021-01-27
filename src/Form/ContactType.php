7ee403395d739b28403d1732ed1eee87665801f5
Florian <floriansarron01@gmail.com>
2021-01-27 10:36:54 +0100

    Création des controllers pour chaque entités

---
+++ src/Form/ContactType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('ville')
+            ->add('codePostal')
+            ->add('telephone')
+            ->add('email')
+            ->add('fonction')
+            ->add('entreprise')
+            ->add('promotion')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
---
+++ src/Form/EntrepriseType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Entreprise;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntrepriseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('adresse')
+            ->add('ville')
+            ->add('codePostal')
+            ->add('telephone')
+            ->add('email')
+            ->add('typeEntreprise')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Entreprise::class,
+        ]);
+    }
+}
