46477ab2d6bc7021ff16e8de7c9fb6608afcddc0
Julien Rouilhac <julien.rouilhac@etu.univ-lyon1.fr>
2021-03-04 15:41:27 +0100

    explicaton texte a remplir

--- src/Form/EntrepriseType.php
+++ src/Form/EntrepriseType.php
@@ -19,37 +19,37 @@ class EntrepriseType extends AbstractType
             [
                 'required'   => true,
                 'label' => 'Nom',
-                'attr' => ['placeholder' => 'Nom de l\'entreprise']
+                'attr' => ['placeholder' => 'Nom de l\'entreprise','title'=>'Entrez le nom','autocomplete'=>'given-name']
             ])
             ->add('adresse',TextType::class,
             [
                 'required'   => true,
                 'label' => 'Adresse',
-                'attr' => ['placeholder' => 'Adresse de l\'entreprise']
+                'attr' => ['placeholder' => 'Adresse de l\'entreprise','title'=>'Entrez l\'adresse','autocomplete'=>'given-name']
             ])
             ->add('ville',TextType::class,
             [
                 'required'   => true,
                 'label' => 'Ville',
-                'attr' => ['placeholder' => 'Location de l\'entreprise']
+                'attr' => ['placeholder' => 'Location de l\'entreprise','title'=>'Entrez la ville','autocomplete'=>'given-name']
             ])
             ->add('codePostal',TextType::class,
             [
                 'required'   => true,
                 'label' => 'Code Postal',
-                'attr' => ['placeholder' => 'CP de l\'entreprise']
+                'attr' => ['placeholder' => 'CP de l\'entreprise','title'=>'Entrez le code postal','autocomplete'=>'given-name']
             ])
             ->add('telephone',TextType::class,
             [
                 'required'   => true,
                 'label' => 'Téléphone',
-                'attr' => ['placeholder' => 'Numéro de l\'entreprise']
+                'attr' => ['placeholder' => 'Numéro de l\'entreprise','title'=>'Entrez le numéro de téléphone','autocomplete'=>'given-name']
             ])
             ->add('email',EmailType::class,
             [
                 'required'   => true,
                 'label' => 'E-mail',
-                'attr' => ['placeholder' => 'E-mail de l\'entreprise']
+                'attr' => ['placeholder' => 'E-mail de l\'entreprise','title'=>'Entrez l\'email','autocomplete'=>'given-name']
             ])
             ->add('typeEntreprise',ChoiceType::class,
             [
