918603314933dc592fc92ab42259a1eb5a2cc055
Julien Rouilhac <julien.rouilhac@etu.univ-lyon1.fr>
2021-05-07 20:48:25 +0200

    debut import

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -69,6 +69,46 @@ class ContactController extends AbstractController
     }
 
     /**
+     * @Route("/import")
+     */
+    public function import(Request $request): Response
+    {
+        $upload     = new UploadData();
+        $formUpload = $this->createForm(UploadType::class, $upload);
+        $formUpload->handleRequest($request);
+        if ($formUpload->isSubmitted() && $formUpload->isValid()) {
+
+            $file     = $upload->getUpload();
+            $fileName = $request->get('file');
+            $file->move($this->getParameter('upload_directory'), $fileName);
+            $upload->setUpload($fileName);
+
+            $reader = Reader::createFromPath(__DIR__.'/'.$fileName);
+            $result = $reader->fetchAssoc();
+            foreach ($result as $row){
+                $contact = new Contact();
+                $contact
+                    ->setNom($row['civility'])
+                    ->setPrenom($row['last_name'])
+                    ->setAdresse($row['first_name'])
+                    ->setVille($row['email'])
+                    ->setCodePostal($row['number'])
+                    ->setTelephone($row['dealer'])
+                    ->setEmail($row['dealer_zone'])
+                    ->setFonction($row['dealer_code'])
+                    ->setEntreprise($row['dealer_code'])
+                    ->setPromotion($row['dealer_code'])
+                ;
+                $this->em->persist($cont);
+
+            }
+            $this->em->flush();
+            $this->addFlash('success','Bien ajouté avec succès');
+        }
+        return $this->redirectToRoute('contact_index');
+    }
+
+    /**
      * @Route("/new", name="contact_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
